bdb01f1a479db30fa9c326cb754537b3840a2500
Bambo Bello <user@Bambo.local>
2023-12-06 05:21:03 +0100

    final

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -4,46 +4,34 @@ import BackButton from '../../components/BackButton';
 import Spinner from '../../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 
-const EditBook = () => {
+const CreateBook = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [publishYear, setpublishYear] = useState('')
   const [loading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-  const { id } = useParams()
+  const {enqueueSnackbar} = useSnackbar();
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios.get(`http://localhost:5555/books/${id}`)
-      .then((response) => {
-        setAuthor(response.data.author);
-        setpublishYear(response.data.publishYear)
-        setTitle(response.data.title)
-        setIsLoading(false);
-      }).catch((error) => {
-        setIsLoading(false)
-        alert('An Error Occured. Please Retry ');
-        console.log(error)
-      })
-  }, [])
-
-  const handleEditBook = () => {
+  const handleSaveBook = () => {
     const data = {
       title,
       author,
       publishYear
     };
     setIsLoading(true);
-    axios.put(`http://localhost:5555/books/${id}`, data)
+    axios.post('http://localhost:5555/books', data)
       .then(() => {
         setIsLoading(false);
+        enqueueSnackbar('Book Created Successfully', {variant : 'success'})
         navigate('/')
       })
       .catch((error) => {
         setIsLoading(false);
-        alert('An Error Occcured')
+        // alert('An Error Occcured')
+        enqueueSnackbar('Book Error Occured', {variant : 'error'})
         console.log(error)
       })
   }
@@ -53,7 +41,7 @@ const EditBook = () => {
   return (
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Edit Book</h1>
+      <h1 className='text-3xl my-4'>Create Book</h1>
       {loading ? <Spinner /> : ''}
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className="my-4">
@@ -64,18 +52,18 @@ const EditBook = () => {
 
         <div className="my-4">
           <label className='text-xl mr-4 text-gray-500' value={author}>Author</label>
-          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
+          <input type="text" onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
         </div>
 
 
         <div className="my-4">
           <label className='text-xl mr-4 text-gray-500' value={publishYear}>Public Year</label>
-          <input type="text" value={publishYear}  onChange={(e) => setpublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
+          <input type="text" onChange={(e) => setpublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>Save</button>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>Save</button>
       </div>
     </div>
   )
 }
 
-export default EditBook
+export default CreateBook
--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -4,30 +4,51 @@ import BackButton from '../../components/BackButton';
 import Spinner from '../../components/Spinner';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 
-const CreateBook = () => {
+
+const EditBook = () => {
   const [title, setTitle] = useState('')
   const [author, setAuthor] = useState('')
   const [publishYear, setpublishYear] = useState('')
   const [loading, setIsLoading] = useState(false);
   const navigate = useNavigate();
+  const { id } = useParams()
+  const {enqueueSnackbar} = useSnackbar();
 
-  const handleSaveBook = () => {
+
+  useEffect(() => {
+    setIsLoading(true);
+    axios.get(`http://localhost:5555/books/${id}`)
+      .then((response) => {
+        setAuthor(response.data.author);
+        setpublishYear(response.data.publishYear)
+        setTitle(response.data.title)
+        setIsLoading(false);
+      }).catch((error) => {
+        setIsLoading(false)
+        alert('An Error Occured. Please Retry ');
+        console.log(error)
+      })
+  }, [])
+
+  const handleEditBook = () => {
     const data = {
       title,
       author,
       publishYear
     };
     setIsLoading(true);
-    axios.post('http://localhost:5555/books', data)
+    axios.put(`http://localhost:5555/books/${id}`, data)
       .then(() => {
         setIsLoading(false);
+        enqueueSnackbar('Book Edited Successfully', {variant : 'success'})
         navigate('/')
       })
       .catch((error) => {
         setIsLoading(false);
-        alert('An Error Occcured')
+        enqueueSnackbar('Book Error Occured', {variant : 'error'})
         console.log(error)
       })
   }
@@ -37,7 +58,7 @@ const CreateBook = () => {
   return (
     <div className='p-4'>
       <BackButton />
-      <h1 className='text-3xl my-4'>Create Book</h1>
+      <h1 className='text-3xl my-4'>Edit Book</h1>
       {loading ? <Spinner /> : ''}
       <div className='flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto'>
         <div className="my-4">
@@ -48,18 +69,18 @@ const CreateBook = () => {
 
         <div className="my-4">
           <label className='text-xl mr-4 text-gray-500' value={author}>Author</label>
-          <input type="text" onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
+          <input type="text" value={author} onChange={(e) => setAuthor(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
         </div>
 
 
         <div className="my-4">
           <label className='text-xl mr-4 text-gray-500' value={publishYear}>Public Year</label>
-          <input type="text" onChange={(e) => setpublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
+          <input type="text" value={publishYear}  onChange={(e) => setpublishYear(e.target.value)} className='border-2 border-gray-500 px-4 py-4 w-full' />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>Save</button>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleEditBook}>Save</button>
       </div>
     </div>
   )
 }
 
-export default CreateBook
+export default EditBook
